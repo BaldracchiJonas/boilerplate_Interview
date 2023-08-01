f562984b971c23de75162af7a391f36e9df98b16
BaldracchiJonas <baldracchijonas@gmail.com>
2023-08-01 20:14:08 -0300

    add mutiple test to TableReport

--- tests/unit/TableReport.spec.js
+++ tests/unit/TableReport.spec.js
@@ -2,7 +2,6 @@ import { mount } from '@vue/test-utils';
 import TableReport from '@/components/TableReport.vue';
 import columnsName from '@/assets/columnsName.json';
 import store from '@/store/index.js';
-import { DOMParser } from 'xmldom';
 
 function extractColumns(data) {
     const columns = [];
@@ -15,12 +14,6 @@ function extractColumns(data) {
       }
     });
     return columns;
-}
-
-function parseHTMLText(htmlString) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlString, 'text/html');
-    return doc.documentElement.textContent;
 }
 
 describe('TableReport.vue', () => {
@@ -37,9 +30,30 @@ describe('TableReport.vue', () => {
 
         // Assert that table headers are rendered correctly
         const tableHeaders = wrapper.findAll('th');
-        expect(tableHeaders.length).toBe(columnsTitles.length);
+        expect(tableHeaders.length).toBe(columnsTitles.length); // 1 header for each column
         columnsTitles.forEach((title, index) => {
-            expect(tableHeaders[index].text()).toBe(parseHTMLText(title));
+            expect(tableHeaders[index].html()).toContain(title);
         });
+    });
+
+    it('renders TableRow component', () => {
+        const wrapper = mount(TableReport, {
+            global: {
+                plugins: [store],
+            },
+        });
+
+        expect(wrapper.findComponent({ name: 'TableRow' }).exists()).toBe(true);
+    });
+
+    it('should pass the correct props to TableRow component', () => {
+        const wrapper = mount(TableReport, {
+            global: {
+              plugins: [store],
+            },
+        });
+        expect(wrapper.findComponent({ name: 'TableRow' }).props('row').exists()).toBe(true);
+        expect(wrapper.findComponent({ name: 'TableRow' }).props('columns').exists()).toBe(true);
+        expect(wrapper.findComponent({ name: 'TableRow' }).props('gpa').exists()).toBe(true);
     });
 });
