399447aa691573677d991838d6d7cb3d65b41f43
BaldracchiJonas <baldracchijonas@gmail.com>
2023-08-02 00:01:56 -0300

    fix style AcademicFitReport component

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -1,39 +1,63 @@
 <template>
-    <div>
-      <div class="info">
+  <div class="flex justify-between">
+    <div class="flex justify-start p-8">
+      <div class="p-4">
+        <ProfileImage class="w-full h-auto min-w-[7rem] max-w-[10rem]"/>
+      </div>
 
-        <ProfileImage/>
-        
-        <input v-model="athleteNameInput" @blur="updateAthleteName" @keyup.enter="updateAthleteName" />
+      <div class="flex flex-col">
 
-        <ul>
-          <li data-test="sport-text">
-            <label data-test="sport-label">Sport:</label>
-            {{athleteData.sport}}
-          </li>
-          <li data-test="class-text">
-            <label data-test="class-label">Class:</label>
-            {{athleteData.grad_year}}
-          </li>
-          <li data-test="club-text">
-            <label data-test="club-label">Club:</label>
-            {{athleteData.club.name}}
-          </li>
-          <li data-test="high-school-text">
-            <label data-test="high-school-label">High School:</label>
-            {{athleteData.high_school.name}}
-          </li>
-          <li data-test="gpa-text">
-            <label data-test="gpa-label">GPA:</label>
-            {{athleteData.gpa}}
-          </li>
-          <li data-test="desired-major-text">
-            <label data-test="desired-major-label">Desired Major:</label>
-            {{athleteData.major}}
-          </li>
-        </ul>
+        <div class="max-w-md pl-3">
+          <input 
+            v-model="athleteNameInput" 
+            @blur="updateAthleteName" 
+            @keyup.enter="updateAthleteName" 
+            class="max-w-md px-1 py-1 text-xl font-bold text-blue-400 rounded w-min focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+          />
+        </div>
+
+        <div class="flex flex-col justify-center sm:flex-row">
+          <div class="p-4 pt-0">
+            <ul class="space-y-2">
+              <li data-test="sport-text">
+                <label class="font-bold" data-test="sport-label">Sport:</label>
+                {{athleteData.sport}}
+              </li>
+              <li data-test="class-text">
+                <label class="font-bold" data-test="class-label">Class:</label>
+                {{athleteData.grad_year}}
+              </li>
+              <li data-test="club-text">
+                <label class="font-bold" data-test="club-label">Club:</label>
+                {{athleteData.club.name}}
+              </li>
+            </ul>
+            </div>
+
+            <div class="p-4 pt-0">
+            <ul class="space-y-2">
+              <li data-test="high-school-text">
+                <label class="font-bold" data-test="high-school-label">High School:</label>
+                {{athleteData.high_school.name}}
+              </li>
+              <li data-test="gpa-text">
+                <label class="font-bold" data-test="gpa-label">GPA:</label>
+                {{athleteData.gpa}}
+              </li>
+              <li data-test="desired-major-text">
+                <label class="font-bold" data-test="desired-major-label">Desired Major:</label>
+                {{athleteData.major}}
+              </li>
+            </ul>
+            </div>
+        </div>
       </div>
     </div>
+    <div class="justify-start p-8 ex">
+      <img src="../../public/insights.png" alt="Image description" class="h-auto rounded w-52" />
+      <span class="flex justify-end text-lg font-semibold text-gray-900">Academic Fit Report</span>
+    </div>
+  </div>
 </template>
   
 <script setup>
